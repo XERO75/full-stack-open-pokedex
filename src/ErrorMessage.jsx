1ef5e4de69a97ca7f3f3237df5ac97aad2734bfa
xavier <laishaoliang@sina.cn>
2022-02-16 14:23:38 +0800

    update

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-
+import React from 'react';
+console.log('====================================');
+console.log(111);
+console.log('====================================');
 const ErrorMessage = ({ error }) => (
   <div data-testid="error">An error occured: {error.toString()}</div>
-)
+);
 
-export default ErrorMessage
+export default ErrorMessage;
