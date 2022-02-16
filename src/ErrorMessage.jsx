8ed9a66a876df4260dc49114dc78d394592bba2a
xavier <laishaoliang@sina.cn>
2022-02-16 15:19:19 +0800

    ddddd

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-console.log('====================================');
-console.log(111);
-console.log('====================================');
 const ErrorMessage = ({ error }) => (
   <div data-testid="error">An error occured: {error.toString()}</div>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles.css';
-console.log('====================================');
-console.log(1);
-console.log('====================================');
 ReactDOM.render(<App />, document.getElementById('app'));
